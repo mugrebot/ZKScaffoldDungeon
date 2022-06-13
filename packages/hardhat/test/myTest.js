0eeed57003e20c7c05cd00035168adef268768dd
Mugrebot <m00npapi@m00npapis-mbp.lan>
2022-06-13 15:47:53 -0400

    add semaphore

--- packages/hardhat/test/myTest.js
+++ packages/hardhat/test/myTest.js
@@ -1,41 +1,47 @@
-const { ethers } = require("hardhat");
-const { use, expect } = require("chai");
-const { solidity } = require("ethereum-waffle");
+import { ethers } from "hardhat";
+import { expect } from "chai";
+import { Game } from "../typechain";
+import { assert } from "console";
+import { BigNumber, Contract } from "ethers";
 
-use(solidity);
+describe("Game", function () {
+  it("default test", async function () {
+    // const [owner] = await ethers.getSigners();
+    const factory = await ethers.getContractFactory("Game");
+    const contract = await factory.deploy();
 
-describe("My Dapp", function () {
-  let myContract;
+    const provided = "yeethan";
+   
 
-  // quick fix to let gas reporter fetch data from gas station & coinmarketcap
-  before((done) => {
-    setTimeout(done, 2000);
+    const greeting = await contract.createCharacter(provided);
+    assert(await contract.getName() === 'yeethan');
+
+    const boss = await contract.createBoss(20,1,1);
+    assert(await contract.getBoss() > ethers.BigNumber.from(0));
+
+   
+
+    const attack = await contract.Attack();
+    assert(await contract.getBoss() > ethers.BigNumber.from(0));
+
+    const attack2 = await contract.Attack();
+    assert(await contract.getBoss() === ethers.BigNumber.from(0));
+
+    const attack3 = await contract.Attack();
+
+    const claim = await contract.ClaimReward();
+    assert(await contract.getXP() > ethers.BigNumber.from(0));
+
+    const HP1 = await contract.getHP();
+    const heal = await contract.Heal();
+    const HP2 = await contract.getHP();
+    assert(HP2 > HP1);
+
+
+
   });
 
-  describe("YourContract", function () {
-    it("Should deploy YourContract", async function () {
-      const YourContract = await ethers.getContractFactory("YourContract");
+  
 
-      myContract = await YourContract.deploy();
-    });
-
-    describe("setPurpose()", function () {
-      it("Should be able to set a new purpose", async function () {
-        const newPurpose = "Test Purpose";
-
-        await myContract.setPurpose(newPurpose);
-        expect(await myContract.purpose()).to.equal(newPurpose);
-      });
-
-      it("Should emit a SetPurpose event ", async function () {
-        const [owner] = await ethers.getSigners();
-
-        const newPurpose = "Another Test Purpose";
-
-        expect(await myContract.setPurpose(newPurpose))
-          .to.emit(myContract, "SetPurpose")
-          .withArgs(owner.address, newPurpose);
-      });
-    });
-  });
+    
 });
